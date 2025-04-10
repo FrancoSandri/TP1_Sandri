de7139ae5c50e7705a9d90c902991d9d005b3caa
Franco Sandri <francosandriespi@gmail.com>
2025-04-10 16:32:06 -0300

    feat: revisarla fabrica de personajes

--- Ejercicio_3/ejercicio3.cpp
+++ Ejercicio_3/ejercicio3.cpp
@@ -1,96 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+
 #include "ejercicio3.h"
+#include "../Ejercicio_2/armas/ItemMagico.cpp"
+#include "../Ejercicio_2/armas/ArmaCombate.cpp"
+#include "../Ejercicio_2/personajes/Mago.cpp"
+#include "../Ejercicio_2/personajes/Guerrero.cpp"
+#include "../Ejercicio_2/personajes/Clases_Concretas_Personaje.h"
+#include "../Ejercicio_2/armas/Clases_Concretas.h"
 
-// Para obtener un número aleatorio entre a y b (incluidos)
-int randomInt(int a, int b) {
-    return a + rand() % (b - a + 1);
-}
-
-unique_ptr<Arma> PersonajeFactory::crearArmaAleatoria() {
-    int tipoTotal = randomInt(0, 8); // 4 mágicas + 5 combate
-    if (tipoTotal <= 3) { // Armas mágicas
-        TipoArmaMagica tipo = static_cast<TipoArmaMagica>(tipoTotal);
-        switch (tipo) {
-            case TipoArmaMagica::Baston: return make_unique<Baston>();
-            case TipoArmaMagica::LibroHechizos: return make_unique<LibroHechizos>();
-            case TipoArmaMagica::Pocion: return make_unique<Pocion>();
-            case TipoArmaMagica::Amuleto: return make_unique<Amuleto>();
-        }
-    } else { // Armas de combate
-        TipoArmaCombate tipo = static_cast<TipoArmaCombate>(tipoTotal - 4);
-        switch (tipo) {
-            case TipoArmaCombate::HachaSimple: return make_unique<HachaSimple>();
-            case TipoArmaCombate::HachaDoble: return make_unique<HachaDoble>();
-            case TipoArmaCombate::Espada: return make_unique<Espada>();
-            case TipoArmaCombate::Lanza: return make_unique<Lanza>();
-            case TipoArmaCombate::Garrote: return make_unique<Garrote>();
-        }
-    }
-    return nullptr;
-}
-
-unique_ptr<Personaje> PersonajeFactory::crearMagoAleatorio() {
-    TipoMago tipo = static_cast<TipoMago>(randomInt(0, 3));
-    string nombre = "Mago_" + to_string(rand());
+unique_ptr<IPersonaje> PersonajeFactory::crearMagoRandom() {
+    int tipo = rand() % 4;
     switch (tipo) {
-        case TipoMago::Hechicero: return make_unique<Hechicero>(nombre);
-        case TipoMago::Conjurador: return make_unique<Conjurador>(nombre);
-        case TipoMago::Brujo: return make_unique<Brujo>(nombre);
-        case TipoMago::Nigromante: return make_unique<Nigromante>(nombre);
+        case 0: return make_unique<Hechicero>();
+        case 1: return make_unique<Conjurador>();
+        case 2: return make_unique<Brujo>();
+        default: return make_unique<Nigromante>();
     }
-    return nullptr;
 }
 
-unique_ptr<Personaje> PersonajeFactory::crearGuerreroAleatorio() {
-    TipoGuerrero tipo = static_cast<TipoGuerrero>(randomInt(0, 4));
-    string nombre = "Guerrero_" + to_string(rand());
+unique_ptr<IPersonaje> PersonajeFactory::crearGuerreroRandom() {
+    int tipo = rand() % 5;
     switch (tipo) {
-        case TipoGuerrero::Barbaro: return make_unique<Barbaro>(nombre);
-        case TipoGuerrero::Paladin: return make_unique<Paladin>(nombre);
-        case TipoGuerrero::Caballero: return make_unique<Caballero>(nombre);
-        case TipoGuerrero::Mercenario: return make_unique<Mercenario>(nombre);
-        case TipoGuerrero::Gladiador: return make_unique<Gladiador>(nombre);
-    }
-    return nullptr;
-}
-
-void PersonajeFactory::crearPersonajesArmados(vector<unique_ptr<Personaje>>& magos, vector<unique_ptr<Personaje>>& guerreros) {
-    srand(static_cast<unsigned int>(time(nullptr))); // semilla para aleatoriedad
-
-    int cantidadMagos = randomInt(3, 7);
-    int cantidadGuerreros = randomInt(3, 7);
-
-    for (int i = 0; i < cantidadMagos; ++i) {
-        auto mago = crearMagoAleatorio();
-        int cantidadArmas = randomInt(0, 2);
-        for (int j = 0; j < cantidadArmas; ++j) {
-            mago->equiparArma(crearArmaAleatoria().release());
-        }
-        magos.push_back(move(mago));
-    }
-
-    for (int i = 0; i < cantidadGuerreros; ++i) {
-        auto guerrero = crearGuerreroAleatorio();
-        int cantidadArmas = randomInt(0, 2);
-        for (int j = 0; j < cantidadArmas; ++j) {
-            guerrero->equiparArma(crearArmaAleatoria().release());
-        }
-        guerreros.push_back(move(guerrero));
+        case 0: return make_unique<Barbaro>();
+        case 1: return make_unique<Paladin>();
+        case 2: return make_unique<Caballero>();
+        case 3: return make_unique<Mercenario>();
+        default: return make_unique<Gladiador>();
     }
 }
 
-int main() {
-    vector<unique_ptr<Personaje>> magos;
-    vector<unique_ptr<Personaje>> guerreros;
-
-    PersonajeFactory::crearPersonajesArmados(magos, guerreros);
-
-    for (const auto& mago : magos) {
-        cout << mago->getNombre() << ": " << mago->usarArma() << endl;
+IArma* PersonajeFactory::crearArmaRandom() {
+    int tipo = rand() % 9;
+    switch (tipo) {
+        case 0: return new Baston();
+        case 1: return new LibroHechizos();
+        case 2: return new Pocion();
+        case 3: return new Amuleto();
+        case 4: return new HachaSimple();
+        case 5: return new HachaDoble();
+        case 6: return new Espada();
+        case 7: return new Lanza();
+        default: return new Garrote();
     }
-
-    for (const auto& guerrero : guerreros) {
-        cout << guerrero->getNombre() << ": " << guerrero->usarArma() << endl;
-    }
-
-    return 0;
 }
+
+// int main() {
+//     srand(static_cast<unsigned int>(time(nullptr)));
+
+//     int numMagos = 3 + rand() % 5;
+//     int numGuerreros = 3 + rand() % 5;
+
+//     vector<unique_ptr<IPersonaje>> personajes;
+//     vector<unique_ptr<IArma>> armas;
+
+//     for (int i = 0; i < numMagos; ++i) {
+//         auto mago = PersonajeFactory::crearMagoRandom();
+//         IArma* arma = PersonajeFactory::crearArmaRandom();
+//         mago->setArma(arma);
+//         personajes.emplace_back(move(mago));
+//         armas.emplace_back(arma);
+//     }
+
+//     for (int i = 0; i < numGuerreros; ++i) {
+//         auto guerrero = PersonajeFactory::crearGuerreroRandom();
+//         IArma* arma = PersonajeFactory::crearArmaRandom();
+//         guerrero->setArma(arma);
+//         personajes.emplace_back(move(guerrero));
+//         armas.emplace_back(arma);
+//     }
+
+//     cout << "=== Personajes generados aleatoriamente === "<< endl ;
+//     for (auto& p : personajes) {
+//         p->mostrarEstado();
+//     }
+
+//     return 0;
+// }
--- Ejercicio_3/ejercicio3.h
+++ Ejercicio_3/ejercicio3.h
@@ -1,51 +1,13 @@
 #ifndef EJERCICIO3_H
 #define EJERCICIO3_H
-
 #include <memory>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-#include "../Ejercicio_2/ejercicio2.h"
-
-using namespace std;
-
-enum class TipoArmaMagica {
-    Baston,
-    LibroHechizos,
-    Pocion,
-    Amuleto
-};
-
-enum class TipoArmaCombate {
-    HachaSimple,
-    HachaDoble,
-    Espada,
-    Lanza,
-    Garrote
-};
-
-enum class TipoMago {
-    Hechicero,
-    Conjurador,
-    Brujo,
-    Nigromante
-};
-
-enum class TipoGuerrero {
-    Barbaro,
-    Paladin,
-    Caballero,
-    Mercenario,
-    Gladiador
-};
+#include "../Ejercicio_2/personajes/IPersonaje.h"
+#include "../Ejercicio_2/armas/IArma.h"
 
 class PersonajeFactory {
 public:
-    static unique_ptr<Arma> crearArmaAleatoria();
-    static unique_ptr<Personaje> crearMagoAleatorio();
-    static unique_ptr<Personaje> crearGuerreroAleatorio();
-    static void crearPersonajesArmados(vector<unique_ptr<Personaje>>& magos, vector<unique_ptr<Personaje>>& guerreros);
+    static unique_ptr<IPersonaje> crearMagoRandom();
+    static unique_ptr<IPersonaje> crearGuerreroRandom();
+    static IArma* crearArmaRandom();
 };
-
 #endif
